f2b3d297c77e69247e6978133db5af92ca6b8fa0
Alan <agm899@gmail.com>
2021-06-11 22:36:25 -0400

    successful connections detected

--- GMTK2021/Wire.gd
+++ GMTK2021/Wire.gd
@@ -6,6 +6,7 @@ var mouseOver = false
 var lineStarted = false
 var visitedNodes = ["start", "end"]
 var lastNodeEntered
+var validConnection = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,23 +15,28 @@ func _ready():
 
 func _process(delta):
 	#print(mouseOver)
-	if lineStarted:
-		add_point(get_global_mouse_position())
-		if self.points.size() > 2:
-			remove_point(1)
-	elif Input.is_action_pressed("click") and mouseOver == true:
-		lineStarted = true
-		visitedNodes[0] = lastNodeEntered
+	if !validConnection:
 		
-		#while get_point_count() > length:
-		#	remove_point(0)
-	
-	if Input.is_action_just_released("click"):
-		lineStarted = false
-		clear_points()
-		if mouseOver == true:
-			visitedNodes[1] = lastNodeEntered
-			print(visitedNodes)
+		if lineStarted:
+			add_point(get_global_mouse_position())
+			if self.points.size() > 2:
+				remove_point(1)
+		elif Input.is_action_pressed("click") and mouseOver == true:
+			lineStarted = true
+			visitedNodes[0] = lastNodeEntered
+			
+			#while get_point_count() > length:
+			#	remove_point(0)
+		
+		if Input.is_action_just_released("click"):
+			lineStarted = false
+			if mouseOver == true:
+				visitedNodes[1] = lastNodeEntered
+				checkConnection()
+			else:
+				clear_points()
+				#print(visitedNodes)
+		
 	
 		
 func entered(nodeColor):
@@ -42,3 +48,13 @@ func entered(nodeColor):
 func exited():
 	mouseOver = false
 	#print("Exited")
+
+func checkConnection():
+	if visitedNodes[0] == "white" and visitedNodes[1] != "white":
+		validConnection = true
+		clear_points()
+		#GET COORDS FOR PORTS
+		add_point(Vector2(720, 416))
+		add_point(Vector2(100, 100))
+	else:
+		clear_points()
