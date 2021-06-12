4ed7b468d4d363b2cdb432fcb5d5ca1023702739
Zayeem Zaman <zayeem@vt.edu>
2021-06-12 13:24:22 -0400

    Updated background sprite

--- GMTK2021/Minigame.gd
+++ GMTK2021/Minigame.gd
@@ -8,6 +8,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("Background").play()
+	get_node("whitePort/Sprite").play()
 	get_node("redPort/Sprite").play()
 	get_node("bluePort/Sprite").play()
 	get_node("greenPort/Sprite").play()
