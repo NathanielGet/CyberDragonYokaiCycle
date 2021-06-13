c6b6d53f2542f7c5b48aaa7311cb9ec18b4626bf
Alan <agm899@gmail.com>
2021-06-12 22:50:21 -0400

    jammer enemy causes the jam and killing it removes it

--- GMTK2021/Enemy/GeneralEnemy.gd
+++ GMTK2021/Enemy/GeneralEnemy.gd
@@ -16,10 +16,13 @@ var velocity = Vector2()
 var progress = 0
 var cur_spd = 40
 
+signal unjam()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size #TODO update this to play area
 	$Area2D.connect("body_entered", self, "_on_Area2D_body_entered")
+	connect("unjam", get_node("../../../../Minigame"), "disableJammer")
 
 func _physics_process(delta):
 	if !b_is_moving:
@@ -71,4 +74,4 @@ func move():
 	progress = 0
 
 func die():
-	pass
+	emit_signal("unjam")
--- GMTK2021/Enemy/Jammer/JammerEnemy.gd
+++ GMTK2021/Enemy/Jammer/JammerEnemy.gd
@@ -1,9 +1,10 @@
 extends "../GeneralEnemy.gd"
 
-signal jam
+signal jam()
 
 func _ready():
 	health = 3
+	connect("jam", get_node("../../../../Minigame"), "activateJammer")
 	
 func _process(delta):
 	emit_signal("jam")
