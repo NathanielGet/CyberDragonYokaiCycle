ab59c624867b2da461181b73cfb8d7bd84c37dc7
Alan <agm899@gmail.com>
2021-06-12 23:57:02 -0400

    HUD for lives works

--- GMTK2021/HUD/HUD.gd
+++ GMTK2021/HUD/HUD.gd
@@ -4,6 +4,11 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var lives3
+var lives2
+var lives1
+var lives0
+var lifeBar
 
 onready var AMMO_GREEN = $"/root/ProjectileTypes".attack_type.GREEN
 onready var AMMO_BLUE = $"/root/ProjectileTypes".attack_type.BLUE
@@ -13,21 +18,28 @@ onready var ammo_label = $UI/Ammo
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
+	lives3 = preload("res://assets/Lives3.png")
+	lives2 = preload("res://assets/Lives2.png")
+	lives1 = preload("res://assets/Lives1.png")
+	lives0 = preload("res://assets/Lives0.png")
+	
+	lifeBar = $UI/lifeBar
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func displayHealth(health):
-	print("OUCH!")
-	if health == 1:
-		pass
-	elif health == 2:
-		pass
-	elif health == 3:
-		pass
+	#print("OUCH!")
+	match health:
+		3:
+			lifeBar.set_texture(lives3)
+		2:
+			lifeBar.set_texture(lives2)
+		1:
+			lifeBar.set_texture(lives1)
+		0:
+			lifeBar.set_texture(lives0)
 
 func displayAmmo(ammo, type):
 	var ammo_type
