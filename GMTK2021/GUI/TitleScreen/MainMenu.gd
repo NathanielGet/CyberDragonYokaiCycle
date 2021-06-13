d422a5335e005e0c7f9622b6405b4021c15f66e0
Nintendroid1 <nlpsalazar@gmail.com>
2021-06-13 01:55:52 -0400

    TItle Screen

---
+++ GMTK2021/GUI/TitleScreen/MainMenu.gd
@@ -0,0 +1,19 @@
+extends Node
+
+signal start_game
+signal how_to_play
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+#	connect("start_game", get_parent(), "_on_TitleScreen_start_game")
+#	connect("how_to_play", get_parent(), "_on_TitleScreen_how_to_play")
+	pass
+	
+func _on_Play_pressed():
+#	emit_signal("start_game")
+	get_tree().change_scene("res://MainGame/MainGame.tscn")
+
+
+func _on_Instructions_pressed():
+#	emit_signal("how_to_play")
+	get_tree().change_scene("res://GameScreen/GameScreen.tscn")
