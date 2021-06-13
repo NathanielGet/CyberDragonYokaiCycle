72acfb4b60198a70ef6666d496d146c384ee56c7
NathanielGet <n.getzler@gmail.com>
2021-06-12 23:18:23 -0400

    Shooters only fire while moving, and improved fire rate consistency

--- GMTK2021/Enemy/Shooter/ShooterEnemy.gd
+++ GMTK2021/Enemy/Shooter/ShooterEnemy.gd
@@ -1,20 +1,22 @@
 extends "../GeneralEnemy.gd"
 
-export var fire_rate = 60
+export var fire_rate = 1
+var can_fire = true
 var counter = 0
 
 func _ready():
 	health = 2
 
 func _process(delta):
-	if counter % fire_rate == 0:
+	if b_is_moving && can_fire:
 		fire()
-		counter = 0
-	counter += 1
 
 func fire():
+	can_fire = false
 	var proj = projectile.instance()
 	get_node("/root/MainGame").add_child(proj)
 	proj.setup(0)
 	proj.position = Vector2(position.x - 50, position.y)
-	proj.linear_velocity = -Vector2(projectile_speed + velocity.x, velocity.y)
+	proj.linear_velocity = -Vector2(projectile_speed, 0)
+	yield(get_tree().create_timer(fire_rate), "timeout")
+	can_fire = true
